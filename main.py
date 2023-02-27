c2ba80e2972e6f51a20bb2969518560ee8b94054
morit <21.5.2003.ms@gmail.com>
2023-02-27 15:52:09 +0100

    added hashtable

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
-
+from os import listdir
+import random
+def find_csv_filenames( path_to_dir, suffix=".csv" ):
+    filenames = listdir(path_to_dir)
+    return [ filename for filename in filenames if filename.endswith( suffix ) ]
 class Hashtable:
 
     def __init__(self, size):
@@ -7,12 +11,16 @@ class Hashtable:
         self.values = [0] * size
 
     def hash_func(self, key):
-        ascii_code = sum([ord(c) for c in key])
+        ascii_code = sum([ord(c) for c in key]) * random.randint(0,20) # So to values are differnt for similar dates
         return ascii_code % self.size
 
-    def add(self, key, value):
+    def add(self, value, key=None):
+        # if the key is not given it will be the Date
+        if key is None:
+            key = value["Date"]
         hash_code = self.hash_func(key)
         i = 1
+        print(key)
         while self.values[hash_code] != 0:
             i = i**2
             hash_code = (hash_code + i) % self.size
@@ -31,9 +39,13 @@ class Hashtable:
         return self.values[hash_code][1]
 
 
-stock_data = pd.read_csv("MSFT.csv")
+hashtable = Hashtable(1000)
+DAYS = 1000
+stock_history = Hashtable(round(DAYS + DAYS * 0.2))
+for path in find_csv_filenames(r"C:\Users\morit\Documents\MEGAsync\2 Semester\Algos\data"):
+    stock_data = pd.read_csv(f"data/{path}")
+    # put every row in the stockhistory hashmap
+    stock_data.apply(stock_history.add, axis=1)
+    hashtable.add(stock_history, path[:-4])
 
-hashtable = Hashtable(60)
-hashtable.add("MSFT", stock_data)
-
-print(hashtable.search("MSFT"))
+    print(hashtable.search(path[:-4]))
